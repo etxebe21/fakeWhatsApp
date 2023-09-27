fb5f25a44862ed81868f6da52e138680632e2799
Etxebe <mikelgartzia21@gmail.com>
2023-09-27 10:08:14 +0200

    creada carpeta components y tres ficheros dentro

---
+++ App.mjs
@@ -0,0 +1,61 @@
+import React, {Component} from "react";
+import { StyleSheet, View } from "react-native";
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Text } from 'react-native';
+
+export default class App extends Component {
+    render() {
+        return(
+          
+            <View style= {styles.mainContainer}>
+              <View style= {styles.headerContainer}>
+                <View style= {styles.leftHeaderContainer}>
+                  <Text style={styles.logo}>WhatsApp</Text>
+                </View>
+                  <View style= {styles.rightHeaderContainer}>
+                    <Icon name = "search" color= "#fff" size={23} style={styles.icon} />
+                    <Icon name = "more-vert" color= "#fff" size={23} style={styles.icon} />
+
+                </View>
+              </View>
+            </View>
+        )
+    }
+}
+
+
+const styles = StyleSheet.create({
+  mainContainer: {
+      flex: 1,
+      backgroundColor: `#F5FCFF`
+  },
+
+  headerContainer: {
+      flex: 1,
+      flexDirection: "row",
+      justifyContent: "space-between",
+      backgroundColor: "#075e54",
+      alignITems: "center"
+  },
+  contentContainer: {
+    flex: 8
+  },
+  rightHeaderContainer: {
+    flexDirection: "row",
+    alignItems: "flex-end"
+  },
+  leftHeaderContainer: {
+    flexDirection: "row",
+    alignItems: "flex-start"
+  },
+  logo: {
+    color: "#ffffff",
+    fontSize: 18,
+    marginLeft: 10,
+    fontWeight: "bold"
+  },
+  icon: {
+    padding: 5,
+  }
+
+});
