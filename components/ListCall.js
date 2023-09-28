ccb3206c5f1d623b993dcc208f6057a57c542b5c
Etxebe <mikelgartzia21@gmail.com>
2023-09-28 21:35:18 +0200

    [NEW] añadidos iconos de llamada perdida o entrante y ajustadas posiciones de los iconos de ListState

--- components/ListCall.js
+++ components/ListCall.js
@@ -8,90 +8,102 @@ export default class ListCall extends Component {
     render() {
 
         const video_call = this.props.video_call;
+        const missed = this.props.missed;
        
         return (
   
 <View style = {styles.listItemContainer}>
-  <View style = {styles.avatarContainer}>
+    <View style = {styles.avatarContainer}>
       <Image 
           style = {styles.avatar}
           source = {{uri: this.props.image}}
           />
-</View>
+    </View>
 
-<View style = {styles.chatDetailsContainer}>
-  <View style = {styles.chatDetailsContainerWrap}>
-      <View style = {styles.nameContainer}>
-              <Text style = {styles.nameText}>{this.props.first_name}</Text>
-          </View>
-      </View>
-  <View style = {styles.chatDetailsContainerWrap}>
-      <View style = {styles.dateContainer}>
-              <Text style = {styles.dateText}>{this.props.date}    {this.props.time}</Text>
-      </View>   
-  </View>
-    
-        <View style={styles.chatDetailsContainerWrap}>
-            <View style={styles.dateContainer}>
-            {video_call ? (
-                        <Icon name="phone" size={20} color="green" /> 
-                    ) : (
-                        <Icon name="video" size={20} color="green" /> 
-                    )}
+    <View style={styles.chatDetailsContainer}>
+             <View style={styles.chatDetailsContainerWrap}>
+                    <View style={styles.nameContainer}>
+                        <Text style={styles.nameText}>{this.props.first_name}</Text>
+                    </View>
+       
+                    <View style={styles.iconContainer}>
+                            {video_call ? (
+                                <Icon name="phone" size={33} color="green" />
+                            ) : (
+                                <Icon name="video" size={33} color="green" />
+                            )}
+                    </View>
             </View>
-        </View>
-    
-  </View>
-</View>          
-            
+
+            <View style={styles.chatDetailsContainerWrap}>
+                <View style={styles.missedContainer}>
+                    {missed ? (
+                        <Icon name="phone-missed" size={15} color="red" /> 
+                    ) : (
+                        <Icon name="phone" size={15} color="green" /> 
+                    )}
+                
+                    <Text style={styles.dateText}  marginLeft= {6}>
+                        {this.props.date}   {this.props.time}
+                    </Text>
+                </View>
+            </View>
+    </View>
+</View>
         );
     }
 }
 
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
     listItemContainer: {
         flex: 1,
-        flexDirection: "row",
-        padding: 10
+        flexDirection: 'row',
+        padding: 10,
     },
     avatarContainer: {
         flex: 1,
-        alignItems: "flex-start"
+        alignItems: 'flex-start',
     },
     chatDetailsContainer: {
         flex: 4,
-        borderBottomColor: "rgba(92,94,94,0.5)",
-        borderBottomWidth: 0.25
+        borderBottomColor: 'rgba(92,94,94,0.5)',
+        borderBottomWidth: 0.25,
     },
     chatDetailsContainerWrap: {
         flex: 1,
-        flexDirection: "row",
-        padding: 5
+        flexDirection: 'row',
+        justifyContent: 'space-between', 
+        alignItems: 'center', 
+        padding: 5,
     },
     nameContainer: {
-        alignItems: "flex-start",
-        flex: 1
+        flex: 1,
     },
     dateContainer: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "flex-end"
+        justifyContent: 'flex-start',
     },
-    msgContainer: {
-        flex:1
+    iconContainer: {
+        width: 40, 
+        alignItems: 'flex-end', 
+        marginBottom: -20,
+        flexDirection: 'row',
+    },
+    missedContainer: {
+        width: 150, 
+        alignItems: 'flex-end', 
+        flexDirection: 'row',
+        padding: 1
     },
     nameText: {
-        fontWeight: "bold",
-        color: "#000"
-
+        fontWeight: 'bold',
+        color: '#000',
     },
     dateText: {
-        fontSize: 12
+        fontSize: 12,
     },
     avatar: {
         borderRadius: 30,
         width: 60,
-        height: 60
-    }
-
+        height: 60,
+    },
 });
